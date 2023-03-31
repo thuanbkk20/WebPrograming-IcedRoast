bf387f0876d20c4363857f6b923239a9e7f1d851
dinhthong <thong.dangdinh031219@hcmut.edu.vn>
2023-03-31 15:38:17 +0700

    add header and footer

--- app/views/products/detail.php
+++ app/views/products/detail.php
@@ -3,7 +3,8 @@
     !empty($page_title)?$page_title:"No name"
 ?></title>
 
-<h1>Chào bạn, đây là trang product detail</h1>
+<!-- <h1>Chào bạn, đây là trang product detail</h1> -->
+
 <?php
     echo $info;
 ?>
