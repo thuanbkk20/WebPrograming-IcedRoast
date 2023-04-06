a7cf992a77df43f7a8214496c6c12895d6690cc8
dinhthong <thong.dangdinh031219@hcmut.edu.vn>
2023-04-06 23:31:10 +0700

    add code login.php

--- app/views/blocks/header.php
+++ app/views/blocks/header.php
@@ -110,7 +110,7 @@
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
                   <li><a class="dropdown-item" href="#">Action</a></li>
                   <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  <li><a class="dropdown-item" href="site/logout">Đăng xuất</a></li>
                 </ul>
               </div>
           </div>
