181e2beb5cd1c7ccbcc3fedfe3ea661aaf7cb50e
trongphu012345@gmail.com <75671199+trongphuhappy@users.noreply.github.com>
2023-04-06 22:26:40 +0700

    404_PageNotFound

--- app/errors/404.php
+++ app/errors/404.php
@@ -1,1 +1,15 @@
-<h1 style="text-align:center">PAGE NOT FOUND</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iced Roast</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div style="width: 100vw; height: 100vh; overflow: hidden;">
+        <img style="width: 100%; height: 100%; object-fit: cover;" src="/public/assets/images/[IcedRoast]404.png" alt="404 Page Not Found">
+    </div>
+</body>
+</html>
