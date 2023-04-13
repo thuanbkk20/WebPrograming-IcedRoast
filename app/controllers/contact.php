0548da607c58af61fc93c6b48a2d7189c03f7ab9
thuanbkk20 <thuan17082002@gmail.com>
2023-04-13 21:05:34 +0700

    Update

--- app/controllers/contact.php
+++ app/controllers/contact.php
@@ -9,7 +9,7 @@ class contact extends Controller{
     public function index(){
         $this->data['sub_content']['page_title']= "Liên hệ";
         $this->data["content"] = 'contact';
-        
+    
         //Render view
         $this->render('layouts/client_layout', $this->data);
     }
