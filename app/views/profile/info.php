7ce87bcdc7acc4ef1f6b9e9fa9f9d843cffcebe0
dinhthong <thong.dangdinh031219@hcmut.edu.vn>
2023-04-21 00:54:18 +0700

    change_password

--- app/views/profile/info.php
+++ app/views/profile/info.php
@@ -33,8 +33,8 @@
 
 <div class="container">
     <div class="row g-0 page-body mx-1 p-2 m-2">
-        <div class="col-3 col-lg-3 col-md-3 col-sm-6 mt-0 ms-3">
-            <div class="d-flex  flex-row mb-3 col-12">
+        <div class="col-lg-3 col-md-3 col-sm-2 mt-0 ms-3">
+            <div class="d-flex flex-row mb-3 col-12">
             <div class="pe-3">                  
                 <img
                   src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
@@ -89,40 +89,47 @@
             </script>
             </div>
         </div>
-        <div class="col-8 col-lg-8 col-md-8 col-sm-6 mt-0 ms-3">
+        <div class="col-lg-8 col-md-8 col-sm-10 mt-0 ms-3">
             <div class="d-flex flex-column">
                     <div class="d-block mb-3">
                         <h2 class="d-block fw-bold">Hồ sơ của tôi</h2>
                         <span>Quản lý thông tin hồ sơ để bảo mật tài khoản</span>
                     </div>
-                    <div class="d-block mb-1">
-                        <span>Tên đăng nhập: <?php echo $user['username'];?></span>
+                    <div class="mb-2 row">
+                        <span class="col-2">Tên đăng nhập</span>
+                        <span class="col-10"><?php echo $user['username'];?></span>
                     </div>
                     <div>
                     <form method="post" action="<?php echo _WEB_ROOT; ?>/profile">
-                                <div class="form-group">
-                                    <span for="first_name">Tên</span>
-                                    <input type="text" name="first_name" placeholder="Tên"  class="form-control" 
-                                    value="<?php echo empty($old['first_name'])?$user['first_name']:$old['first_name'];?>">
-                                    <span class="text-danger">
-                                        <?php echo empty($errors['first_name'])?false:$errors['first_name']; ?>
-                                    </span> 
+                                <div class="form-group row">
+                                    <label for="first_name" class="col-2 col-form-label">Tên</label>
+                                    <div class="col-10">
+                                        <input type="text" name="first_name" placeholder="Tên"  class="form-control" 
+                                        value="<?php echo empty($old['first_name'])?$user['first_name']:$old['first_name'];?>">
+                                        <span class="text-danger">
+                                            <?php echo empty($errors['first_name'])?false:$errors['first_name']; ?>
+                                        </span> 
+                                    </div>
                                 </div>
-                                <div class="form-group">
-                                <label for="last_name">Họ và tên đệm</label>
+                                <div class="form-group row">
+                                <label for="last_name" class="col-2 col-form-label"> Họ và tên đệm</label>
+                                <div class="col-10"> 
                                     <input type="text" name="last_name" placeholder="Họ và tên đệm" class="form-control"  
                                     value="<?php echo empty($old['last_name'])?$user['last_name']:$old['last_name'];?>">
                                     <span class="text-danger">
                                     <?php echo (empty($errors['last_name'])?false:$errors['last_name']); ?>
                                     </span>
                                 </div>
-                                <div class="form-group">
-                                <label for="phone_number">Số điện thoại</label>
-                                    <input type="text" name="phone_number" placeholder="So dien thoai" class="form-control"  
+                                </div>
+                                <div class="form-group row">
+                                <label for="phone_number" class="col-2 col-form-label">Số điện thoại</label>
+                                <div class="col-10"> 
+                                <input type="text" name="phone_number" placeholder="So dien thoai" class="form-control"  
                                         value="<?php echo empty($old['phone_number'])?$user['phone_number']:$old['phone_number'];?>">
                                         <span class="text-danger">
                                         <?php echo (empty($errors['phone_number'])?false:$errors['phone_number']); ?>
                                     </span>
+                                </div>  
                                 </div>
                                 <button type="submit" class="btn btn-primary">Lưu</button>
                     </form>
