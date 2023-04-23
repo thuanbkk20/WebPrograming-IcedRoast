2a809a6d98952bb5487c24f070fcea2dcb775563
thuanbkk20 <thuan17082002@gmail.com>
2023-04-23 21:19:23 +0700

    Add contact model

---
+++ app/models/ContactModel.php
@@ -0,0 +1,34 @@
+<?php
+/*
+    Ke thua tu class Model
+*/
+class ContactModel extends Model{
+    private $_table = 'contact';
+
+    function __construct(){
+        parent::__construct();
+    }
+
+    function tableFill(){
+        return $this->_table;
+    }
+
+    function fieldFill()
+    {
+        return "*";
+    }
+
+    function primaryKey()
+    {
+        return "id";
+    }
+
+    public function getAll(){
+        $data = $this->db->table($this->_table)->getAll();
+        return $data;
+    }
+
+    public function addContact($data){
+        $this->db->table($this->_table)->insert($data);
+    }
+}
