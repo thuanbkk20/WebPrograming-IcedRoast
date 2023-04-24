d4f7d560205ded8fa1e80d6d9255c43e3762536a
thuanbkk20 <thuan17082002@gmail.com>
2023-04-24 13:50:57 +0700

    Update

--- app/models/ProductModel.php
+++ app/models/ProductModel.php
@@ -87,4 +87,16 @@ class ProductModel extends Model{
         $data = $query->fetch(PDO::FETCH_ASSOC)['image'];
         return $data;
     }
+
+    public function addProduct($data){
+        $this->db->table($this->_table)->insert($data);
+    }
+
+    public function updateProduct($id, $data){
+        $this->db->table($this->_table)->where('id','=',$id)->update($data);
+    }
+
+    public function deleteProduct($id){
+        $this->db->table($this->_table)->where('id','=',$id)->delete();
+    }
 }
