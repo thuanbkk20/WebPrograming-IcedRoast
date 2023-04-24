26c2031d34405b80536012a7de8258d267f3f72e
trongphu012345@gmail.com <75671199+trongphuhappy@users.noreply.github.com>
2023-04-24 14:57:22 +0700

    update UI (permission, favicon, product UI)

--- app/views/layouts/client_layout.php
+++ app/views/layouts/client_layout.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="/public/assets/images/logo_IcedRoast.png">
     <link type="text/css" rel="stylesheet" href="/public/assets/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
