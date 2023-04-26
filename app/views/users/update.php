f7deb2236c7ee75de7351fbde78ab5a64957c69a
thuanbkk20 <thuan17082002@gmail.com>
2023-04-26 08:57:41 +0700

    Add update page

--- app/views/users/update.php
+++ app/views/users/update.php
@@ -1,7 +1,80 @@
+<h1 class="d-flex justify-content-between " style="margin-left: 700px;" >Chỉnh sửa tài khoản</h1>
+
+<form class="p-4 border rounded mx-auto" method="post" action="<?php echo _WEB_ROOT.'/admin/UserModify/update';?>" style="width:42%; background-color: rgb(194, 241, 200);">
+    <!-- form content -->
+    <?php echo '<pre>'; print_r($userToUpdate); echo '</pre>';?>
+    <div>
+        <span >
+            <?php echo (empty($msg)?false:$msg); ?>
+        </span>
+    </div>
+    <!-- form content -->
+    <input type="hidden" name="id" value="<?php echo $userToUpdate['id']; ?>">
+    <div class="form-group mb-4">
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" class="form-control" 
+        value="<?php echo (!empty($old['email'])?$old['email']:$userToUpdate['username']);?>"/>
+        <span class="text-danger">
+            <?php echo (empty($errors['email'])?false:$errors['email']); ?>
+        </span>
+    </div>
+
+    <div class="form-group mb-4">
+        <label for="name">Tên</label>
+        <input type="text" id="name" name="first_name" class="form-control" 
+        value="<?php echo (!empty($old['first_name'])?$old['first_name']:$userToUpdate['first_name']);?>"/>
+        <span class="text-danger">
+            <?php echo (empty($errors['first_name'])?false:$errors['first_name']); ?>
+        </span>
+    </div>
+
+    <div class="form-group mb-4">
+        <label for="name">Họ và tên đệm</label>
+        <input type="text" id="name" name="last_name" class="form-control" 
+        value="<?php echo (!empty($old['last_name'])?$old['last_name']:$userToUpdate['last_name']);?>"/>
+        <span class="text-danger">
+            <?php echo (empty($errors['last_name'])?false:$errors['last_name']); ?>
+        </span>
+    </div>
+
+    <div class="form-group mb-4">
+        <label for="phonenumber">Số điện thoại</label>
+        <input type="text" id="phonenumber" name="phone_number" class="form-control" 
+        value="<?php echo (!empty($old['phone_number'])?$old['phone_number']:$userToUpdate['phone_number']);?>"/>
+        <span class="text-danger">
+            <?php echo (empty($errors['phone_number'])?false:$errors['phone_number']); ?>
+        </span>
+    </div>
+
+    <div class="form-group mb-4">
+        <label for="role">Quyền</label>
+        <select id="role" name="role" class="form-control">
+            <option value="user" <?php if(isset($old['role'])&&$old['role']=='user') echo 'selected="selected"'; ?>>Người dùng</option>
+            <option value="admin" <?php if(isset($old['role'])&&$old['role']=='admin') echo 'selected="selected"'; ?>>Admin</option>
+        </select>
+    </div>
+
+    <div class="form-group mb-4">
+        <label for="password">Mật khẩu</label>
+        <input type="password" id="password" name="password" placeholder="Nhập mật khẩu" class="form-control" 
+        value="<?php echo (empty($old['password'])?false:$old['password']);?>"/>
+        <span class="text-danger">
+            <?php echo (empty($errors['password'])?false:$errors['password']); ?>
+        </span>
+    </div>
+
+    <div class="form-outline mb-4">
+        <label for="password">Xác nhận mật khẩu</label>
+        <input type="password" id="repeatpassword" name="confirm_password" placeholder="Nhập lại mật khẩu" class="form-control form-control-lg" 
+        value="<?php echo (empty($old['confirm_password'])?false:$old['confirm_password']);?>"/>
+        <span >
+    <?php echo (empty($errors['confirm_password'])?false:$errors['confirm_password']); ?>
+    </span>
+    </div>
 
     <div class="d-flex justify-content-center">
         <button type="submit"
-        class="btn btn-primary">Cập nhật</button>
+        class="btn btn-primary">Chỉnh sửa tài khoản</button>
     </div>
 </form>
 <br/><br/>
